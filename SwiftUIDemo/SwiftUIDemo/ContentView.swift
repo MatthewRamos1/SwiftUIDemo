1c7aec873028ba7f464a44b6d5a9c139bbdf11a2
Matthew Ramos <matthewramos@Matthews-MacBook-Pro.local>
2019-09-24 15:51:27 -0400

    added xcode project

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Matthew Ramos on 9/24/19.
+//  Copyright © 2019 Matthew Ramos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
